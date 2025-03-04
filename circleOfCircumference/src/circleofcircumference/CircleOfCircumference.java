4adddc676a316285009f2af7de4dc6c2b4128c10
LeahBasson <leahbasson54@gmail.com>
2025-03-04 18:32:34 +0200

    circle of circumference & area

---
+++ circleOfCircumference/src/circleofcircumference/CircleOfCircumference.java
@@ -0,0 +1,16 @@
+
+package circleofcircumference;
+
+
+public class CircleOfCircumference {
+
+    
+    public static void main(String[] args) {
+        int radius = 6;
+        double Circumference = 2 * Math.PI * radius;
+        double Area = Math.PI* radius * radius;
+        System.out.println(Circumference);
+        System.out.println(Area);
+    }
+    
+}
