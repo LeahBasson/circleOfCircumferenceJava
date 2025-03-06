5e20c8f650115ed0cee46b1fe5f9f1051d867cd4
LeahBasson <leahbasson54@gmail.com>
2025-03-06 18:07:12 +0200

    :fire:

--- circleOfCircumference/src/circleofcircumference/CircleOfCircumference.java
+++ circleOfCircumference/src/circleofcircumference/CircleOfCircumference.java
@@ -1,14 +1,21 @@
 
 package circleofcircumference;
 
+import java.util.Scanner;
+
 
 public class CircleOfCircumference {
 
     
     public static void main(String[] args) {
-        int radius = 6;
-        double Circumference = 2 * Math.PI * radius;
-        double Area = Math.PI* radius * radius;
+        Scanner scn = new Scanner(System.in);
+        int radius;
+        double Circumference,Area;
+        
+        System.out.print("Enter a radius: ");
+        radius = scn.nextInt();
+        Circumference = 2 * Math.PI * radius;
+        Area = Math.PI* radius * radius;
         System.out.println(Circumference);
         System.out.println(Area);
     }
